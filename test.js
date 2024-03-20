c7834fddcc906620190cb8eac4e220d0c2a334b9
tropaadigselv <frederikahorup@gmail.com>
2024-03-20 09:39:48 +0100

    Lavede vidre på hovedside

--- test.js
+++ test.js
@@ -3,6 +3,19 @@ all = document.getElementById("alle");
 fav = document.getElementById("Favoriter");
 ny = document.getElementById("Ny");
 
+// variabler for modal
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".Modal_Open");
+const closeModal = document.querySelector(".Modal_close");
+// sættter lytter der lytter efter klik for at vise modal og genstarter stjerner
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+// lukker modal på samme måde som for at vise modal
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
 function toProfile() {
   pro.style.display = "block";
   all.style.display = "none";
