026e55e98874abbf2754e7a3e85227c5f2f171da
August Hedman <augustlh@icloud.com>
2024-04-17 09:33:05 +0200

    Entry-view done

--- server/public/scripts/home.js
+++ server/public/scripts/home.js
@@ -1,10 +1,23 @@
 let entriesData = [];
+let selectedEntry = null;
 
 function getVaults(callback){
     fetch('/vaults').then(response => response.json()).then(data => {
         const vaults = data.vaults;
         callback(vaults);
     });
+}
+
+function decryptEntry(entry, masterpassword){
+   //check if masterpassword is correct
+   //if not update some text to say that the password is incorrect
+   //if correct decrypt the entry
+}
+
+function deleteEntry(entry){
+}
+
+function editEntry(entry){
 }
 
 function getEntries(callback){
@@ -38,10 +51,10 @@ function updateEntriesInfo(entries){
         websitePara.classList.add('entry-website');
         usernamePara.classList.add('entry-username');
 
-        entry.website = entry.website.split('.')[0];
-        entry.website = entry.website.charAt(0).toUpperCase() + entry.website.slice(1);
+        let website = entry.website.split('.')[0];
+        website = website.charAt(0).toUpperCase() + website.slice(1);
 
-        websitePara.textContent = "" + entry.website;
+        websitePara.textContent = "" + website;
         usernamePara.textContent = "" + entry.username;
 
         entryDiv.appendChild(websitePara);
@@ -83,6 +96,11 @@ document.addEventListener('DOMContentLoaded', function(){
     getUserDetails(updateUserInfo);
     getVaults(updateVaultsInfo);
     getEntries(updateEntriesInfo);
+
+    var entryInfoElement = document.querySelector('.entry-info-container');
+    if (!selectedEntry) {
+        entryInfoElement.classList.add('hidden');
+    }
     
 });
 
@@ -92,9 +110,15 @@ document.querySelector('.entries').addEventListener('click', function(event) {
         document.querySelectorAll('.entry').forEach(entry => {
             entry.classList.remove('entry-selected');
         });
-
+        
+        selectedEntry = entriesData[clickedEntry.dataset.web_id];
         clickedEntry.classList.add('entry-selected');
-
-        console.log(entriesData[clickedEntry.dataset.web_id]);
+        const entryInfoElement = document.querySelector('.entry-info-container');
+        //update entry info
+        document.querySelector('#entry-website').textContent =  entriesData[clickedEntry.dataset.web_id].website;
+        document.querySelector('#entry-username').textContent = selectedEntry.username;
+        document.querySelector('#entry-title').textContent = selectedEntry.website.split('.')[0].charAt(0).toUpperCase() + selectedEntry.website.split('.')[0].slice(1);
+        entryInfoElement.classList.remove('hidden');
+        //console.log(entriesData[clickedEntry.dataset.web_id]);
     }
 });
